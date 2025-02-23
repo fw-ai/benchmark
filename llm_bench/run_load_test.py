1b9a394498f0470be00f25b06164ad4f94a40bc1
Tanjiro <tushar.goel.ml@gmail.com>
2025-02-23 22:47:48 +0530

    Update run_load_test.py

--- llm_bench/run_load_test.py
+++ llm_bench/run_load_test.py
@@ -1,9 +1,10 @@
 import os
 
-user, input_tok, output_tok = 1, 100, 500
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiYzkxZWIzMjgtZjRkMS00MjhkLThlMWUtYjUwY2E1NTNkMTg5IiwiZXhwIjoxNzU5MTUxMDE1LCJvcmdfdXVpZCI6IjI4MDM2MDY4LWEzNjMtNDk5Ni1iMWJiLWIyYzJhZjk0ZTdiZiJ9.-h7EMVq51Y67DQ7zlNgeP9oubSDwqzig8zWrGQgi63g"
-url = "http://localhost:30000/v1"
+model_name = "llama3p3-70b-instruct-fp8-engine"
+user, input_tok, output_tok = 1, 6000, 350
+token = "sk-xxxxxxx"
+url = "http://localhost:8000/v1"
 
 os.system(
-    f"locust -t 2min -u {user} -r {user} -o {output_tok} -H {url} -p {input_tok} --api-key {token} --model=llama3_1 --prompt-randomize --chat --stream --provider openai --temperature 0.2 --summary summary.txt"
+    f"locust -t 2min -u {user} -r {user} -o {output_tok} -H {url} -p {input_tok} --api-key {token} --model={model_name} --prompt-randomize --chat --stream --provider openai --temperature 0.2 --summary summary.txt"
 )
