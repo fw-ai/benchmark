a572f41ce4b5555ac00c429f743c1b4df91af6b3
yacineb <yacine@adaptive-ml.com>
2025-03-05 11:24:35 +0100

    ⚡️ change run priority, adaptive first

--- llm_bench/entrypoint.sh
+++ llm_bench/entrypoint.sh
@@ -18,17 +18,17 @@ mkdir -p $OUTPUT_DIR/nocache
 mkdir -p $OUTPUT_DIR/perfectcache
 mkdir -p $OUTPUT_DIR/reports
 
-echo "vllm - 100% cache hit rate"
-./launch_all.sh -s $OUTPUT_DIR/perfectcache/$PREFIX-vllm.csv -u $VLLM_ENDPOINT
-
-echo "vllm - 0% cache hit rate (randomized beginning tokens for each prompt)"
-./launch_all.sh -s $OUTPUT_DIR/nocache/$PREFIX-vllm.csv -u $VLLM_ENDPOINT -r
-
 echo "adaptive - 100% cache hit rate"
 ./launch_all.sh -s $OUTPUT_DIR/perfectcache/$PREFIX-adaptive.csv -u $ADAPTIVE_ENDPOINT -p adaptive -m test -k $ADAPTIVE_API_KEY
 
 echo "adaptive - 0% cache hit rate (randomized beginning tokens for each prompt)"
 ./launch_all.sh -s $OUTPUT_DIR/nocache/$PREFIX-adaptive.csv -u $ADAPTIVE_ENDPOINT -p adaptive -m test -k $ADAPTIVE_API_KEY -r
+
+echo "vllm - 100% cache hit rate"
+./launch_all.sh -s $OUTPUT_DIR/perfectcache/$PREFIX-vllm.csv -u $VLLM_ENDPOINT
+
+echo "vllm - 0% cache hit rate (randomized beginning tokens for each prompt)"
+./launch_all.sh -s $OUTPUT_DIR/nocache/$PREFIX-vllm.csv -u $VLLM_ENDPOINT -r
 
 echo "Generate plots"
 python plotting.py --model Llama-3.1-8b --output-tokens 128 --input-files $OUTPUT_DIR/nocache/$PREFIX-adaptive.csv $OUTPUT_DIR/nocache/$PREFIX-vllm.csv --output-file $OUTPUT_DIR/reports/$PREFIX-vllm-adaptive-nocache.html --extra-header "Adaptive $ADAPTIVE_VERSION vs. vllm 0.7.1 (randomized prompts)"
