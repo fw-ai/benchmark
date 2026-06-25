5612e9a279f138811c3aa362e291974a6c43151a
Dmitry Maliuga <dmaliuga@fireworks.ai>
2026-06-25 21:43:43 +0000

    fixing dirname

--- llm_bench/estimators/bandwidth/models/__init__.py
+++ llm_bench/estimators/bandwidth/models/__init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from llm_bench.estimators.bandwidth.modes.deepseek4 import estimate_deepseek4_bandwidth
+from llm_bench.estimators.bandwidth.models.deepseek4 import estimate_deepseek4_bandwidth
 
 __all__ = ["estimate_deepseek4_bandwidth"]
 
